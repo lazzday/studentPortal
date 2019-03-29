e1a1d22a2208880c7f79754e23be09d100c3a96e
Liam Day <dayliam1@gmail.com>
2019-03-29 14:07:18 +0000

    Add individual subject content page Add downloadable file functionality Add timetable page Add icons

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,19 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Noodle Home'});
 });
 
+/* GET Timetable page. */
+router.get('/timetable', function (req, res, next) {
+    console.log(req.url);
+    res.render('timetable', {title: 'Timetable'});
+});
+
 
 /* GET individual subject page*/
 router.get('/module_*', function (req, res, next) {
     var urlStrings = req.url.split('_');
     var subject = urlStrings[urlStrings.length - 1];
     LearningResource.find({'subject': subject}, function (err, docs) {
+        console.log(docs);
         res.render('subject', {title: subject, resources: docs});
     });
 });
